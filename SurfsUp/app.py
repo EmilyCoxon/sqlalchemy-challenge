b029d374a45440a94a7be9d900865c1ab697cd9f
Emily Coxon <emilyecoxon@gmail.com>
2022-12-05 18:37:41 +0800

    finished part 2

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -44,9 +44,10 @@ app = Flask(__name__)
 def welcome():
     return(
         f"Available Routes:<br/>":
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/holiday<br/>"
     )
 
 
@@ -54,29 +55,75 @@ def welcome():
 # Convert the query results to a dictionary by using date as the key and prcp as the value.
 
 @app.route("/api/v1.0/precipitation")
-
+def precip():
     session = Session(engine)
+    date_prcp_results = session.query(Measurement.date,Measurement.prcp).\
+        filter(Measurement.date > '2016-08-23').all()
     
+    session.close()
     
+    date_prcp = list(np.ravel(date_prcp_results))
 
 # Return the JSON representation of your dictionary.
 
+    return jsonify = list(date_prcp)
+
 ### 3. /api/v1.0/stations
-# Return a JSON list of stations from the dataset.
+
+@app.route("/api/v1.0/stations")
+def station():
+    session = Session(engine)
+    station_results = session.query(Measurement.station).distinct().all()
+    
+    session.close()
+    
+    stations = list(np.ravel(station_results))
+    # return json representation of dict
+     return jsonify(stations)
+
 
 ### 4. /api/v1.0/tobs
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    tobs_results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date > '2016-08-23').\
+        filter(Measurement.station == "USC00519281").all()
+    session.close()
+
 # Query the dates and temperature observations of the most-active station for the previous year of data.
 
+    tobs_date_station = list(np.ravel(tobs_results))
+    
 # Return a JSON list of temperature observations for the previous year.
+    
+    return jsonify(tobs_date_station)
 
 ### 5. /api/v1.0/<start> and /api/v1.0/<start>/<end>
 # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
-
-# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
-
 # For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
 
-##### Hints
-##### Join the station and measurement tables for some of the queries.
+@app.route(f"/api/v1.0/holiday")
+def holiday(start_date = '2017-07-23', end_date='2017-08-23'):
 
-# Use the Flask jsonify function to convert your API data to a valid JSON response object.
+ # For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+    
+    session = Session(engine)
+    holiday_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    session.close()
+
+    holiday_tobs = []
+    for min, avg, max in holiday_results:
+        hol_dict = {}
+        hol_dict["Min"] = min
+        hol_dict["Average"] = avg
+        hol_dict["Max"] = max
+        holday_tobs.append(hol_dict)
+
+    return jsonify(holiday_tobs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
